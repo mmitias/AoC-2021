204be450996417c17fa0047cf030071566f5c546
Mark Mitias <mark@tcgplayer.com>
2021-12-14 14:09:00 -0500

    Update day14b.py
    Cleaned up the code for part 2 significantly

--- day14b.py
+++ day14b.py
@@ -6,66 +6,46 @@ def readFile(fileName):
         fileObj.close()
         return lines
 
-def countPairs(pairlist):
-    global pairs
-    for pair in pairlist:
-        pairs[pair] += 1
-
-inputFile = readFile("day14.txt")
-
-polymer = inputFile[0]
 pairs = {}
 chars = {}
 addchar = []
 pairlist = []
-polychars = list(polymer)
-
-for char in polychars:
-    if char in chars:
-        chars[char] += 1
-    else: 
-        chars[char] = []
-        chars[char] = 1
-
-for idx in range(2, len(inputFile)):
-    instruct = inputFile[idx]
-    instruct = instruct.split(" -> ")
-    key = instruct[0]
-    pairs[key] = []
-    pairs[key] = 0
-    pairlist.append(instruct[0])
-    addchar.append(instruct[1])
-
-startpairs = []
 depth = 40
 
-for idx, polychar in enumerate(polychars):
-    if idx < len(polychars)-1: 
-        pair = polychar + polychars[idx+1]
-        startpairs.append(pair)
+inputFile = readFile("day14.txt")
+polymer = list(inputFile[0])
 
-countPairs(startpairs)
-temppairs = pairs.copy()
+for row in inputFile[2:]:
+    key, value = row.split(" -> ")
+    pairs[key] = 0
+    pairlist.append(key)
+    addchar.append(value)
+    chars[value] = 0
+
+for char in polymer:
+    chars[char] += 1
+
+for idx, char in enumerate(polymer[:-1]):
+    pair = char + polymer[idx+1]
+    pairs[pair] += 1
+
+pairscopy = pairs.copy()
 
 for idx in range(depth):
     for pair, count in pairs.items():
         index = pairlist.index(pair)
-        nextchar = addchar[index]
-        newpair = pair[0]+nextchar
-        newpair2 = nextchar+pair[1]        
-        #print(pair, newpair, newpair2, pairs[pair])
-        if pairs[pair]:
-            temppairs[pair] -= pairs[pair]
-            temppairs[newpair] += pairs[pair]
-            temppairs[newpair2] += pairs[pair]
-            if nextchar in chars:
-                chars[nextchar] += pairs[pair]
-            else: 
-                chars[nextchar] = []
-                chars[nextchar] = 1
-    pairs = temppairs.copy()
+        insertchar = addchar[index]
+        leftpair = pair[0] + insertchar
+        rightpair = insertchar + pair[1]        
 
-cvalues = chars.values()
-most = max(cvalues)
-least = min(cvalues)
+        if pairs[pair]:
+            pairscopy[pair] -= pairs[pair]
+            pairscopy[leftpair] += pairs[pair]
+            pairscopy[rightpair] += pairs[pair]
+            chars[insertchar] += pairs[pair]
+
+    pairs = pairscopy.copy()
+
+most = max(chars.values())
+least = min(chars.values())
 print(most - least)
